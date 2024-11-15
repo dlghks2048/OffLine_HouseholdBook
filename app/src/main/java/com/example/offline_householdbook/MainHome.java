54f8ad9801d932740151cc148bbd5cbbc40f0584
dlghks <dlghks2048@gmail.com>
2024-11-15 19:50:09 +0900

    MainHome crystal

--- app/src/main/java/com/example/offline_householdbook/MainHome.java
+++ app/src/main/java/com/example/offline_householdbook/MainHome.java
@@ -1,6 +1,11 @@
 package com.example.offline_householdbook;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainHome extends AppCompatActivity {
+    Button CalendarButton, ReportButton, SettingButton;
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +26,32 @@ public class MainHome extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        CalendarButton = findViewById(R.id.btn_calendar);
+        ReportButton = findViewById(R.id.btn_graph);
+        SettingButton = findViewById(R.id.btn_settings);
 
+        CalendarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CalendarHome.class);
+                startActivity(intent);
+            }
+        });
 
+        ReportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ReportActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        SettingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SettingActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
