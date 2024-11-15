422ee2b40a001803e2e94a43e7da55b82fa8779f
dlghks <dlghks2048@gmail.com>
2024-11-15 19:42:15 +0900

    Activity Intent crystal

--- app/src/main/java/com/example/offline_householdbook/MainHome.java
+++ app/src/main/java/com/example/offline_householdbook/MainHome.java
@@ -19,5 +19,7 @@ public class MainHome extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
     }
 }
